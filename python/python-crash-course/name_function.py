95ec8af8634b90d9e7e56ef3b688cdafcc0b9695
Qiang Guo <guoqiangshu@163.com>
2019-03-17 16:27:17 +0800

    update python crash noet

--- python/python-crash-course/name_function.py
+++ python/python-crash-course/name_function.py
@@ -3,7 +3,10 @@ def get_formatted_name_(first, last):
     full_name = first + ' ' + last
     return full_name.title()
 
-def get_formatted_name(first, middle, last):
+def get_formatted_name(first, last, middle=''):
     """生成整洁的姓名"""
-    full_name = first + ' ' + middle + ' ' + last
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last
     return full_name.title()
---
+++ python/python-crash-course/test_survey.py
@@ -0,0 +1,46 @@
+import unittest
+from survey import AnonymousSurvey
+
+class TestAnonmyousSurvey(unittest.TestCase):
+
+    def setUp(self):
+        """
+        创建一个调查对象和一组答案，供使用的测试方法使用
+        """
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+
+    def test_store_single_response(self):
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
+
+    def test_store_three_responses(self):
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+
+    '''
+    """针对AnonymousSurvey类的测试"""
+    def test_store_single_response(self):
+        """测试单个答案会被妥善地存储"""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        my_survey.store_response('English')
+        self.assertIn('English', my_survey.responses)
+
+    def test_store_three_responses(self):
+        """测试三个答案会被妥善地存储"""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
+    '''
+
+unittest.main()
