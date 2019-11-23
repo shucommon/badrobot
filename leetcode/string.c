c5194ca910ee27a9aef71e82217fad1b61f38a04
Qiang, Guo <guoqiangshu@163.com>
2019-11-23 12:06:26 +0800

    add string replace

--- leetcode/string.c
+++ leetcode/string.c
@@ -15,7 +15,6 @@ char * test( char ** dic, int size, char *sentence)
         }
         it++;
     }
-    printf("cnt %d\n", cnt);
 
     char ** arr = (char **)malloc(sizeof(char *) * cnt);
     int n = 0;
@@ -24,28 +23,44 @@ char * test( char ** dic, int size, char *sentence)
     while(sscanf(sentence + len, "%s ", tmp) != EOF) {
         arr[n] = (char *)malloc(sizeof(char) * (strlen(tmp) + 1));
         strcpy(arr[n], tmp);
-        printf("%s\n", arr[n]);
         n++;
         len += (strlen(tmp) + 1);
     }
 
     for(int i = 0; i < size; i++) {
+        for(int j = 0; j < n; j++) {
+            char *index;
+            if(((index = strstr(arr[j], dic[i])) != NULL) && (index == &arr[j][0])) {
+                strcpy(arr[j], dic[i]);
+            }
+        }
     }
 
+    int l = 0;
+    char *ret = (char *)malloc(sizeof(char) * 1001001);
+    for(int i = 0; i < cnt; i++) {
+        strcpy(ret + l, arr[i]);
+        l += strlen(arr[i]);
+        if(i < n - 1) {
+            *(ret + l) = ' ';
+            l++;
+        }
+    }
+    *(ret + l) = '\0';
 
-    char *ret;
     return ret;
 }
 
 int main()
 {
     char ** dic = (char **)malloc(sizeof(char *) * 3);
-    dic[0] = "abc"; 
-    dic[1] = "def"; 
-    dic[2] = "xyz"; 
-    int size = 10;
-    char *sen = "abcd defg xyza";
+    dic[0] = "cat";
+    dic[1] = "bat";
+    dic[2] = "rat";
+    int size = 3;
+    char *sen = "the cattle was rattled by the battery";
     char *ret = test(dic, size, sen);
+    printf("%s\n", ret);
 
     return 0;
 }
